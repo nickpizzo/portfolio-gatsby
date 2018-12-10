769edc3304b48f26214b7cbc683b171d22fb6fc1
Nick Pizzo <nick.pizzo@gmail.com>
2018-12-09 20:10:41 -0800

    config tweaks to try fixes for netlify build failing

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-source-drupal`,
-      options: {
-        baseUrl: `http://dev-vegan-transitions.pantheonsite.io/`,
-        apiBase: `jsonapi`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [`cabin`, `Open Sans`],
@@ -31,9 +24,18 @@ module.exports = {
         name: `Gatsby Portfolio`,
         short_name: `GatsbyJS`,
         start_url: `/`,
+        display: `minimal-ui`,
       },
     },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-drupal`,
+      options: {
+        typeName: 'DrupalGraqhQL',
+        baseUrl: `http://dev-vegan-transitions.pantheonsite.io/`,
+        apiBase: `jsonapi`,
+      },
+    },
   ],
 };
